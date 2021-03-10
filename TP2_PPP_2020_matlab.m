8e5e0423add0dfd64afc89b4e6004d8bdb7a2c70
Rubens Benevides <52086242+RubensBenevides@users.noreply.github.com>
2021-03-10 19:06:37 -0300

    Update TP2_PPP_2020_matlab.m
    Adicionei mais comentários às equações, muita coisa envolvida

--- TP2_PPP_2020_matlab.m
+++ TP2_PPP_2020_matlab.m
@@ -1,5 +1,8 @@
 clc
 clear
+%********************************************************
+% INICIALIZAR CONSTANTES E PARÂMETROS
+%********************************************************
 D = 0 ; H = 11; M = 13; S = 45;     % Dia, hora, minuto, segundo da época GPS
 GM_T = 3.986008E14;                 % cte. geocêntrica TERRA
 GM_S = 1.3271244004193938E20;       % cte. geocêntrica SOL
@@ -59,18 +62,18 @@ L2=[84653910.02547;83790961.72746;
     97090462.70744;88057229.08846;
     87761627.83746;93486219.20545;
     97107947.98744];
-% Combinação ion-free
+% Combinação ion-free para corrigir o erro ionosférico na propagação das ondas:
 PD_L0   = (m1*C1)-(m2*P2);
 Fase_L0 = (m1*L1)-(m2f*L2);
 Lb      = [PD_L0;Fase_L0];
 RMSEx = zeros(100,1);
-% Pesos das observáveis
+% Pesos das observáveis que será utilizado no ajustamento (minimização das funções):
 P_PD    = eye(7)*inv(3)^2;   % Inverso do quadrado da precisão da PD
 P_fase  = eye(7)*inv(0.1)^2; % Inverso do quadrado da precisão da Fase
 Peso    = [P_PD    zeros(7); % Peso = matriz diagonal 14x14 com pesos
            zeros(7) P_fase];
            
-% ATRASO NOS HARDWARES (TGD) (tabelado no ppte1971.12n)
+% ATRASO NOS HARDWARES (TGD) (tabelado no arquivo  RINEX ppte1971.12n)
 TGD = [-1.07102096081E-08;
        -1.11758708954E-08;
        -1.67638063431E-08;
@@ -89,9 +92,9 @@ tt_GPS = tr-(PD_L0./c);
 % INTERPOLAÇÃO DE XsYsZs E dts EM CADA tt_GPS
 %*********************************************************        
 XYZsp3 = dlmread('XsYzZs_sp3.csv'); % Ler coordenadas XsYsZs do arquivo excel
-epocas_sp3 = [(36900:900:43200)]; % Define 8 epocas variando 900s (4 antes, 4 depois)
+epocas_sp3 = [(36900:900:43200)];   % Define 8 epocas variando 900s (4 antes, 4 depois)
 dts_clk = dlmread('ts_clk05s.csv'); % Lê tempos do relógio do arquivo excel
-epocas_clk_5 = [(40410:5:40445)]; % Define 8 epocas variando 5s (4 antes, 4 depois)
+epocas_clk_5 = [(40410:5:40445)];   % Define 8 epocas variando 5s (4 antes, 4 depois)
 for n=1:SATs
     % Organizar os dados lidos para usar na interpolação
     coorX=XYZsp3(n:SATs:8*SATs,1)*1000; 
@@ -132,7 +135,7 @@ for n=1:SATs
      zen(n,1)  = pi-alfa;
      elev(n,1) = rad2deg((pi/2)-zen(n)); 
 end
-% Modelo de Saastamoinen sem dR (como Bernise 5.0)
+% Modelo de Saastamoinen para correção do erro de propagação na Troposfera:
 Trop = 0.002277*inv(1+D)*sec(zen).*(P+((1255/T)+0.05)*e-B*tan(zen).^2);
 
 %**********************************************************
@@ -162,7 +165,7 @@ GAST = GAST*15; Hsol = GAST-RA_sol; Hlua = GAST-RA_lua;
 Xsol = UA*cosd(DEC_sol)*cosd(Hsol);
 Ysol = UA*cosd(DEC_sol)*sind(Hsol);
 Zsol = UA*sind(DEC_sol); Rsol = UA; rsol = [Xsol,Ysol,Zsol]/Rsol;
-% Calcular distância média terra-lua
+% Calcular distância média terra-lua com o semi-eixo e a excentricidade da órbita:
 a = 384400; e = 0.05490; b = a*sqrt(1-e^2); Rlua = sqrt(a*b)*1000;
 % LUA: coordenadas, vetor, versores e magnitude
 Xlua = Rlua*cosd(DEC_lua)*cosd(Hlua);
@@ -171,10 +174,10 @@ Zlua = Rlua*sind(DEC_lua); rlua = [Xlua,Ylua,Zlua]/Rlua;
 % Coordenadas da estação
 r_est = [3687624.3674;-4620818.6827;-2386880.3805];
 R_est = norm(r_est); rest = r_est/R_est;
-% Números de Love
+% Números de Love (elasticidade da Terra):
 h0 = 0.6078 ; l0 = 0.0847; h2 = -0.0006; l2 = 0.0002;
 h2 = h0+h2*(3*(sind(lat)^2)-1)/2; l2 = l0+l2*(3*(sind(lat)^2)-1)/2;
-% Correção SOL
+% Correção para o efeito de maré do SOL:
 for n=1:3
     AA = (GM_S/GM_T)*(R_est^4/Rsol^3);
     BB = (3*l2*rsol(n)*rest(n)*rsol(n));
@@ -183,7 +186,7 @@ for n=1:3
     EE = -0.025*sind(lat)*cosd(lat)*sind(GAST+long)*rest(n);
     MareSol(n,1) = AA*(BB+(CC-DD)*rest(n))+EE;
 end
-% Correção LUA
+% Correção para o efeito de maré da LUA:
 for n=1:3
     AA = (GM_L/GM_T)*(R_est^4/Rlua^3);
     BB = (3*l2*rlua(n)*rest(n)*rlua(n));
@@ -197,7 +200,7 @@ mare=MareLua+MareSol;
 %*******************************************************
 % ERRO RELATIVÍSTICO
 %*******************************************************
-% 1 - Carregar parâmetros orbitais de cada PRN
+% 1 - Carregar parâmetros orbitais de cada PRN (cada satélite)
 a_e_toe_vn_m0 = dlmread('a_e_toe_vn_m0.csv');
 a_e_toe_vn_m0;
 sqr_a = a_e_toe_vn_m0(:,1); % a^1/2
@@ -205,12 +208,12 @@ excen = a_e_toe_vn_m0(:,2); % exentricidade
 toe   = a_e_toe_vn_m0(:,3); % toe = 360000
 vn    = a_e_toe_vn_m0(:,4); % variação do mov. ang. médio
 M0    = a_e_toe_vn_m0(:,5); % anomalia média inicial
-% 2 - Propagar os parâmetros para tt_GPS
+% 2 - Propagar os parâmetros para tt_GPS (tempo de transmissão dos satélites):
 for n=1:SATs 
     nk(n,1)=(((GM_T/(sqr_a(n))^6))^(1/2))+vn(n); % mov. ang. no toe
     tk(n,1)=tt_GPS(n)-toe(n);                
     Mk(n,1)=M0(n)+(nk(n)*tk(n));              
-    % Anomalia média de cada PRN pelo método de Newton
+    % Calcular anomalia média de cada PRN pelo método iterativo de Newton:
     Ek0(n,1)=Mk(n,1)+excen(n,1)*Mk(n,1);
     Ek1(n,1)=Mk(n,1)+excen(n,1)*Ek0(n,1);
     while abs(Ek1(n,1)-Ek0(n,1))>1e-15; 
@@ -220,7 +223,7 @@ for n=1:SATs
     % 3 - Salvar correção
     dtR(n,1)=F*excen(n)*sqr_a(n)*sin(Ek1(n));
 end
-% CORREÇÃO DE RELATIVIDADE E ATRASO DE HARDWARE NOS dts (dtsC)
+% CORREÇÃO DE RELATIVIDADE E ATRASO DE HARDWARE NOS dts (dtsC = dts Corrigido):
 dtsC_L1 = dts_GPS+dtR-TGD;
 dtsC_L2 = dts_GPS+dtR-(TGD.*1.647); 
 % Combinação do dts em L0
@@ -234,7 +237,7 @@ while (~fim)
     itera = itera+1;
     % tempo de voo da onda (tal_L0) combinação na PD
     tal_L0 = (PD_L0./c)-dtr+dtsC_L1;
-    % tempo de transmissão da cada GPS refinado
+    % tempo de transmissão da cada GPS refinado:
     ttGPS_L0_ref = tr-dtr+dtsC_L1-tal_L0;
     % Interpolar XsYsZs e dts novamente em ttGPS_L0_ref
     for n=1:SATs
@@ -244,7 +247,7 @@ while (~fim)
         Xs_ref(n,1)=spline(epocas_sp3,coorX,ttGPS_L0_ref(n));
         Ys_ref(n,1)=spline(epocas_sp3,coorY,ttGPS_L0_ref(n));
         Zs_ref(n,1)=spline(epocas_sp3,coorZ,ttGPS_L0_ref(n));
-        % Interpolação do dts
+        % Interpolação do dts:
         dts_inter=dts_clk(n:SATs:8*SATs,1)';
         %dtsC_L0(n,1) = spline(epocas_clk_5,dts_inter,ttGPS_L0_ref(n));      
     end
@@ -260,7 +263,7 @@ while (~fim)
     FASE = psr + Xo(4) - dtsC_L1*c + Xo(5:end)*0.19 + Trop;
     L0 = [PD;FASE];
     L  = L0 - Lb;
-    % Componentes da matriz A
+    % Componentes da matriz A (Matriz Jacobiana, derivadas das equações em relação às observações):
     A = [(Xo(1)-Xs_rot(1))/psr(1) (Xo(2)-Ys_rot(1))/psr(1) (Xo(3)-Zs_rot(1))/psr(1) 1 0 0 0 0 0 0 0; 
          (Xo(1)-Xs_rot(2))/psr(2) (Xo(2)-Ys_rot(2))/psr(2) (Xo(3)-Zs_rot(2))/psr(2) 1 0 0 0 0 0 0 0;
          (Xo(1)-Xs_rot(3))/psr(3) (Xo(2)-Ys_rot(3))/psr(3) (Xo(3)-Zs_rot(3))/psr(3) 1 0 0 0 0 0 0 0;
@@ -277,7 +280,7 @@ while (~fim)
          (Xo(1)-Xs_rot(7))/psr(7) (Xo(2)-Ys_rot(7))/psr(7) (Xo(3)-Zs_rot(7))/psr(7) 1 0 0 0 0 0 0 0.19];
     N   = A'*Peso*A;
     U   = A'*Peso*L;
-    Xc  = -inv(N)*U; % Correção dos parâmetros
+    Xc  = -inv(N)*U; % A correção dos parâmetros está aqui em cada iteração
     Xa  = Xo+Xc;
     Xo  = Xa;
     dtr = Xo(4)/c;
@@ -285,8 +288,8 @@ while (~fim)
     fim = (max(abs(Xc))<1.0E-4) || (itera > 10);
 end
 itera
-gl=14-11; % graus de liberdadde
-V=A*Xc+L  % resíduos das observações (Pseudodistâncias e Fases)
+gl=14-11; % graus de liberdadde (nº de eq. menos o nº de variáveis) 
+V=A*Xc+L  % resíduos das observações (ou seja, as pseudodistâncias e fases)
 La=Lb+V;  % Observações ajustadas    
 var_posteriori=V'*Peso*V/gl  % Variância a posteriori
 mvcXa=var_posteriori*inv(N); % MVC dos parâmetros (X,Y,Z,dt,fase1,...,faseN)
@@ -299,9 +302,11 @@ if qui_quadrado < qui_quadrado_tabelado_95
 else
    fprintf('Falhou no teste qui-quadrado (valor calculado = %.2f > 7.82 valor tabelado)\n', qui_quadrado);
 end
-% Métricas de Erro
+% Comparar valor calculado com o verdadeiro:
 Erro_x = Xa(1)-(3687624.3674)
 Erro_y = Xa(2)-(-4620818.6827)
 Erro_z = Xa(3)-(-2386880.3805)
+# Métrica dada pela norma euclidiana:
 Erro_xyz = norm([Erro_x,Erro_y,Erro_z])
-EQM = sqrt((Erro_xyz^2+var_posteriori^2)/2) % média quadrática
+% Média quadrática do erro total com a variância à posteriori:
+EQM = sqrt((Erro_xyz^2+var_posteriori^2)/2)
